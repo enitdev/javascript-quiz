01c881d22dde8a18cc9b5bd09783ca2d7422c1f5
enitial <spam@literecords.com>
2022-01-14 15:07:30 +0000

    complete draft

--- js/lib.js
+++ js/lib.js
@@ -3,6 +3,25 @@ const delayInMilliseconds15 = 1500; //! 1.5 second
 let questionNum = 0;
 let qCorrect = 0;
 
+function yourScore(qTitle, qScore, qFeedback) {
+    if (qCorrect == 9 | 10) {
+        document.getElementById(qTitle).innerHTML = "Superstar DJ";
+        document.getElementById(qScore).innerHTML = qCorrect;
+        document.getElementById(qFeedback).innerHTML = "That was amazing! Where did you gather all this knowledge from?! I'm sure we'll be seeing you headling events in the future";
+    } else if (qCorrect == 6 | 7 | 8) {
+        document.getElementById(qTitle).innerHTML = "Great DJ";
+        document.getElementById(qScore).innerHTML = qCorrect;
+        document.getElementById(qFeedback).innerHTML = "That was some impressive score. Have you thought about being doing a mobile disco? I really think you could WOW at parties";
+    } else if (qCorrect == 4 | 5) {
+        document.getElementById(qTitle).innerHTML = "DJ in the making";
+        document.getElementById(qScore).innerHTML = qCorrect;
+        document.getElementById(qFeedback).innerHTML = "We've seen better, we've seen worse. With a little grit and determination you could be behind the decks in no time.";
+    } else {
+        document.getElementById(qTitle).innerHTML = "novice";
+        document.getElementById(qScore).innerHTML = qCorrect;
+        document.getElementById(qFeedback).innerHTML = "Have you thought about a career in retail?";
+    }
+}
 function changeQ(qID) {
     let id = document.getElementById(qID).style.display = "none";
     qID++;
@@ -14,6 +33,19 @@ function answerChosen(div, num) {
     let z = document.getElementById(x).innerHTML
     let id1val = document.getElementById(x).style.backgroundColor = "#BF8D13";
     decision(x, y, z);
+}
+// Question 10 answerChosen
+function answerChosen10(div, num, qTitle, qScore, qFeedback) {
+    let x = div.id;
+    let y = num;
+    let z = document.getElementById(x).innerHTML;
+    let id1val = document.getElementById(x).style.backgroundColor = "#BF8D13";
+    decision(x, y, z);
+        //
+    let a = qTitle.id;
+    let b = qScore.id;
+    let c = qFeedback.id;
+    yourScore(a, b, c);
 }
 function decision(brderId, frameId, answer) {
     switch(frameId) {
