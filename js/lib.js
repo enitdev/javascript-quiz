55548c48068f03c5a4eb95ce4eb22d71d7be1d89
enitial <spam@literecords.com>
2022-01-14 00:34:43 +0000

    added delay to answer reveal

--- js/lib.js
+++ js/lib.js
@@ -1,12 +1,19 @@
+const delayInMilliseconds = 1500; //1.5 second
+
 function answerChosen(div) {
     let x = div.id;
     let z = document.getElementById(x).innerHTML
+    let id1val = document.getElementById(x).style.backgroundColor = "#BF8D13";
     decision(x, z);
 }
 function decision(brder, answer) {
     if (answer == "Calvin Harris") {
-        let id1val = document.getElementById(brder).style.backgroundColor = "#1C9816";
+        setTimeout(function() {
+            let id1val = document.getElementById(brder).style.backgroundColor = "#1C9816";
+        }, delayInMilliseconds);
     } else {
+        setTimeout(function() {
         let id1val = document.getElementById(brder).style.backgroundColor = "#982216";
+        }, delayInMilliseconds);
     }
 }
